555de4cb28af7153ec2fa3c3e25f074070ce67a3
Raisson Carvalho <raissoncarvalho94@gmail.com>
2023-10-16 20:27:09 -0300

    changing models and admin

--- acessos/admin.py
+++ acessos/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 
 from acessos.models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+  pass
 
 # Register your models here.
 admin.site.register(CustomUser)
--- acessos/models.py
+++ acessos/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 
 from django.contrib.auth.models import AbstractUser, Group
-from django.db import models
 
 
 class CustomUser(AbstractUser):
