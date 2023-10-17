734584b7667c29ebf93b2e7b3f8f021038b95d92
Wilton Santana <wsantana3@gmail.com>
2023-10-17 12:40:15 -0300

    commmit dois

--- acessos/admin.py
+++ acessos/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-
-
+from django.contrib.auth.admin import UserAdmin
 from acessos.models import CustomUser
 
 # Register your models here.
-admin.site.register(CustomUser)
+
+
+class CustomUserAdmin(UserAdmin):
+    pass
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
